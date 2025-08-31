6d58af0a5537a82b36de7558105f172aa5c433f7
Marc Hefter <marchefter@gmail.com>
2025-08-31 16:54:50 +0200

    fixed some typos and bugs

--- docker/appuser/docker-entrypoint.sh
+++ docker/appuser/docker-entrypoint.sh
@@ -9,14 +9,12 @@ APP_GID=$(/usr/bin/id -g mosquitto)
 if [ "$(/usr/bin/id -u)" != '0' ]; then
 	# some debugging info
 	echo "running as: $(/usr/bin/id)"
-elif [[ "${HOST_GID}" != "${APP_GID}" -o "${HOST_UID}" != "${APP_UID}" ]]; then
+elif [[ "${HOST_GID}" != "${APP_GID}" ]] || [[ "${HOST_UID}" != "${APP_UID}" ]]; then
 	# change user/group to HOST_UID/HOST_GID
-	/usr/sbin/groupmod --gid "${HOST_GID}" mosquitto 2>/dev/null || true
-	/usr/sbin/usermod --uid "${HOST_UID}" --gid mosquitto mosquitto 2>/dev/null || true
+	[[ "${HOST_GID}" != "${APP_GID}" ]] && /usr/sbin/groupmod --gid "${HOST_GID}" mosquitto 2>/dev/null || true
+	[[ "${HOST_UID}" != "${APP_UID}" ]] && /usr/sbin/usermod --uid "${HOST_UID}" --gid mosquitto mosquitto 2>/dev/null || true
 	# change owner of /app
-	[ -d "/app" ] && /bin/chown --recursive mosquitto:mosquitto /mosquitto 2>/dev/null || true
-else
-	# nothing to do
+	[ -d "/mosquitto" ] && /bin/chown --recursive mosquitto:mosquitto /mosquitto 2>/dev/null || true
 fi
 
 # execute CMD
@@ -24,6 +22,7 @@ if [ "$(/usr/bin/id -u)" != '0' ]; then
 	# already running as unprivileged user
 	exec "$@"
 else
-	# drop from root to appuser
-	/usr/bin/setuidgid appuser "$@"
+	[ -x /usr/bin/setuidgid ] || apk --no-cache add daemontools-encore
+	# drop from root to mosquitto
+	/usr/bin/setuidgid mosquitto "$@"
 fi
